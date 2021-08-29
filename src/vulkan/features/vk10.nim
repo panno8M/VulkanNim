9db4f906789575c38701875c8c4ca7fd1e079ff3
panno <panno.800k@gmail.com>
2021-08-29 09:03:54 +0900

    Fix bitmask operations

--- src/vulkan/features/vk10.nim
+++ src/vulkan/features/vk10.nim
@@ -1,4 +1,4 @@
-# Generated at 2021-08-28T12:30:12Z
+# Generated at 2021-08-29T00:03:32Z
 # vulkan 1.0
 # Vulkan core API interface definitions
 # =====================================
@@ -4742,6 +4742,58 @@ import strformat
 import strutils
 import sets
 
+proc `==`*[Flagbits: enum](a, b: Flags[Flagbits]): bool =
+  a.uint32 == b.uint32
+
+macro `toFlagSets`*[Flagbits: enum](Type: typedesc[Flagbits]; bits: varargs[untyped]): HashSet[Flagbits] =
+  if (repr Type).find("FlagBits") == -1:
+    macros.error("Expect the enum that has the suffix Flagbits, got " & repr Type)
+  # var resultStr = "{DebugUtilsMessageSeverityFlagBitsEXT.VerboseBitExt}"
+  var resultStr = "toHashSet(["
+  for bit in bits: resultStr.add "{repr Type}.{bit},".fmt
+  resultStr.add "])"
+  return resultStr.parseStmt
+template `{}`*[Flagbits: enum](Type: typedesc[Flagbits]; bits: varargs[untyped]): HashSet[Flagbits] =
+  Type.toFlagSets(bits)
+
+proc `toFlagSets`*[Flagbits: enum](flags: Flags[Flagbits]): HashSet[Flagbits] =
+  var val = 1.uint32
+  let flags = flags.uint32
+  while val <= flags:
+    if (val and flags) != 0:
+      result.incl Flagbits(val)
+    val = val shl 1
+template `{}`*[Flagbits: enum](flags: Flags[Flagbits]): HashSet[Flagbits] =
+  flags.toFlagSets
+
+proc `toFlags`*[Flagbits: enum](flagsets: HashSet[Flagbits]): Flags[Flagbits] =
+  for flagset in flagsets:
+    result = result or flagset
+template `<+>`*[Flagbits: enum](flagsets: HashSet[Flagbits]): Flags[Flagbits] =
+  flagsets.toFlags
+
+proc `toFlags`*[Flagbits: enum](flagbits: Flagbits): Flags[Flagbits] =
+  Flags[Flagbits](flagbits)
+template `<+>`*[Flagbits: enum](flagbits: Flagbits): Flags[Flagbits] =
+  flagbits.toFlags
+
+proc `all`*[Flagbits: enum](Type: typedesc[Flags[Flagbits]]): Flags[Flagbits] =
+  Flags[Flagbits]((Flagbits.high.ord-1) shl 1)
+template `all`*[Flagbits: enum](flags: Flags[Flagbits]): Flags[Flagbits] =
+  flags.typeof.all
+
+proc `all`*[Flagbits: enum](Type: typedesc[HashSet[Flagbits]]): HashSet[Flagbits] =
+  Flags[Flagbits].all.toFlagSets
+template `all`*[Flagbits: enum](flagsets: HashSet[Flagbits]): HashSet[Flagbits] =
+  flagsets.typeof.all
+
+proc `none`*[Flagbits: enum](Type: typedesc[HashSet[Flagbits]]): HashSet[Flagbits] = return
+template `none`*[Flagbits: enum](flagsets: HashSet[Flagbits]): HashSet[Flagbits] =
+  flagsets.typeof.none
+proc `none`*[Flagbits: enum](Type: typedesc[Flags[Flagbits]]): Flags[Flagbits] = return
+template `none`*[Flagbits: enum](flags: Flags[Flagbits]): Flags[Flagbits] =
+  flags.typeof.none
+
 proc `and`*[Flagbits: enum](a, b: Flagbits): Flags[Flagbits] =
   Flags[Flagbits](a.uint32 and b.uint32)
 proc `and`*[Flagbits: enum](a: Flags[Flagbits]; b: Flagbits): Flags[Flagbits] =
@@ -4769,37 +4821,16 @@ template `xor`*[Flagbits: enum](a: Flagbits; b: Flags[Flagbits]): Flags[Flagbits
 proc `xor`*[Flagbits: enum](a, b: Flags[Flagbits]): Flags[Flagbits] =
   Flags[Flagbits](a.uint32 xor b.uint32)
 
-proc `not`*[Flagbits: enum](a: Flags[Flagbits]): Flags =
-  Flags[Flagbits](not a.uint32)
+proc `not`*[Flagbits: enum](flags: Flags[Flagbits]): Flags[Flagbits] =
+  flags xor flags.all
 
-macro `toFlagSets`*[Flagbits: enum](Type: typedesc[Flagbits]; bits: varargs[untyped]): HashSet[Flagbits] =
-  if (repr Type).find("FlagBits") == -1:
-    macros.error("Expect the enum that has the suffix Flagbits, got " & repr Type)
-  # var resultStr = "{DebugUtilsMessageSeverityFlagBitsEXT.VerboseBitExt}"
-  var resultStr = "toHashSet(["
-  for bit in bits: resultStr.add "{repr Type}.{bit},".fmt
-  resultStr.add "])"
-  return resultStr.parseStmt
-template `{}`*[Flagbits: enum](Type: typedesc[Flagbits]; bits: varargs[untyped]): HashSet[Flagbits] =
-  Type.toFlagSets(bits)
+template `not`*[Flagbits: enum](flagbits: Flagbits): Flags[Flagbits] =
+  not flagbits.toFlags
 
-proc `toFlagSets`*[Flagbits: enum](flags: Flags[Flagbits]): HashSet[Flagbits] =
-  var val = 1.uint32
-  let flags = flags.uint32
-  while val <= flags:
-    if (val and flags) != 0:
-      result.incl Flagbits(val)
-    val = val shl 1
-template `{}`*[Flagbits: enum](flags: Flags[Flagbits]): HashSet[Flagbits] =
-  flags.toFlagSets
-
-proc `toFlags`*[Flagbits: enum](flagsets: HashSet[Flagbits]): Flags[Flagbits] =
-  for flagset in flagsets:
-    result = result or flagset
-template `+`*[Flagbits: enum](flagsets: HashSet[Flagbits]): Flags[Flagbits] =
-  flagsets.toFlags
+template `not`*[Flagbits: enum](flagsets: HashSet[Flagbits]): HashSet[Flagbits] =
+  (not flagsets.toFlags).toFlagSets
 
 proc `toString`*[Flagbits: enum](flags: Flags[Flagbits]): string =
-  $flags{}
+  $flags.toFlagSets
 proc `$`*[Flagbits: enum](flags: Flags[Flagbits]): string =
   flags.toString
