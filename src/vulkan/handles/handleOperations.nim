f6c53aef49d455a64e894b5c5dc497119626222c
panno <panno.800k@gmail.com>
2022-01-04 18:39:47 +0900

    fix handle operation

--- src/vulkan/handles/handleOperations.nim
+++ src/vulkan/handles/handleOperations.nim
@@ -11,6 +11,7 @@ proc `==`*[T](a, b: Handle[T]): bool = a.uint64 == b.uint64
 proc `==`*[T](a, b: NonDispatchableHandle[T]): bool = a.uint64 == b.uint64
 
 proc isValid*[T](h: Handle[T]): bool = h.uint64 != 0
+proc isValid*[T](h: NonDispatchableHandle[T]): bool = h.uint64 != 0
 
 
 template parent*(prt: untyped) {.pragma.}
