9063662d5545dfee5d8f4ec97302e58b4b92c6c9
panno <panno.800k@gmail.com>
2022-02-24 18:05:38 +0900

    fix type of handle

--- src/vulkan/handles/handle_operations.nim
+++ src/vulkan/handles/handle_operations.nim
@@ -2,16 +2,16 @@ from ../enums import ObjectType
 template nonDispatchable* {.pragma.}
 
 type
-  Handle*[T: static ObjectType] = distinct uint64
-  NonDispatchableHandle*[T: static ObjectType] = distinct uint64
+  Handle*[T: static ObjectType] = distinct pointer
+  NonDispatchableHandle*[T: static ObjectType] = distinct pointer
 
 
-proc `==`*[T](a, b: Handle[T]): bool = a.uint64 == b.uint64
+proc `==`*[T](a, b: Handle[T]): bool = a.pointer == b.pointer
 
-proc `==`*[T](a, b: NonDispatchableHandle[T]): bool = a.uint64 == b.uint64
+proc `==`*[T](a, b: NonDispatchableHandle[T]): bool = a.pointer == b.pointer
 
-proc isValid*[T](h: Handle[T]): bool = h.uint64 != 0
-proc isValid*[T](h: NonDispatchableHandle[T]): bool = h.uint64 != 0
+proc isValid*[T](h: Handle[T]): bool = h.pointer != nil
+proc isValid*[T](h: NonDispatchableHandle[T]): bool = h.pointer != nil
 
 
 template parent*(prt: untyped) {.pragma.}
